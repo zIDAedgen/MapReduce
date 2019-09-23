11e2187fc0af3681226f77ca19a195d233c09d00
z5171092 <z5171092@cse.unsw.edu.au>
2019-09-24 08:42:05 +1000

    ElasticSearch

---
+++ src/main/scala/index.scala
@@ -0,0 +1,152 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import java.io._
+import scala.xml.XML
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Set
+import scalaj.http._
+import play.api.libs.json._
+
+object CaseIndex {
+    def main(args: Array[String]) {
+        
+        // create an index
+        val index_response = Http("http://localhost:9200/legal_idx").method("PUT").header("Content-Type", "application/json").option(HttpOptions.connTimeout(10000)).option(HttpOptions.readTimeout(10000)).asString
+        // create a new mapping
+        val mapping_response = Http("http://localhost:9200/legal_idx/cases/_mapping?pretty").postData("""{"cases":{"properties":{"id":{"type":"text"},"name":{"type":"text"},"url":{"type":"text"},"catchphrase":{"type":"text"},"sentence":{"type":"text"},"person":{"type":"text"},"location":{"type":"text"},"organization":{"type":"text"}}}}""").method("PUT").header("Content-Type", "application/json").option(HttpOptions.connTimeout(10000)).option(HttpOptions.readTimeout(10000)).asString
+    
+        // get input path from argument
+        val inputFile = args(0)
+        // get list of case files
+        //val all_files = getListOfFiles(inputFile)
+       
+        val d = new File(inputFile)
+         if (d.exists && d.isDirectory) {
+             d.listFiles.filter(_.isFile).toList
+         } else {
+             List[File]()
+         }
+        
+        
+        
+        
+        
+        
+        
+        // processing each file
+        all_files.foreach(file=>{
+            //println(file)
+            // load XML
+            val xml = XML.loadFile(file)
+
+            // get filename
+            val filename = file.getName().split("\\.")(0)
+
+            // get all information from XML
+            val name = (xml \ "name").text.filter(_ >= ' ')
+            val url =  (xml \ "AustLII").text.filter(_ >= ' ')
+            
+            
+            
+            //Process the catchphrase
+            val catchphrases = (xml \ "catchphrases" \ "catchphrase")
+            val catchphrase = new StringBuilder(""); 
+            catchphrases.foreach(_catchphrase=>{
+                catchphrase ++=  _catchphrase.text + " "
+            })
+            
+            
+            //Process the sentences
+            val sentences = (xml \ "sentences" \ "sentence")
+            val sentence_list = new ListBuffer[String]()
+            sentences.foreach(sentence => {
+                sentence_list += sentence.text.replace("\"","\\\"")
+
+            })
+
+            
+            
+            // Create set to store each name entities
+            //val locations : Set[String] = Set()
+            //val people : Set[String] = Set()
+            //val organizations : Set[String] = Set()
+            //val general_terms : Set[String] = Set()
+            val locations = wordProcess()
+            val people = wordProcess()
+            val organizations = wordProcess()
+           
+            
+            
+            
+            // process each sentence  
+            sentence_list.foreach(e_sentence=>{
+                var parameterNLP = Http("""http://localhost:9000/?properties=%7B'annotators':'ner','outputFormat':'json'%7D""").postData(e_sentence).method("POST").header("Content-Type", "application/json").option(HttpOptions.connTimeout(60000)).option(HttpOptions.readTimeout(60000)).asString.body
+            
+                // parse reponse to JSON object
+                val NLP_json: JsValue = Json.parse(parameterNLP)
+                
+                // get all entities
+                val tokens = NLP_json \\ "tokens"
+                tokens.foreach(token=>{
+                    val text = token; val ner = token; var idx = 0;
+
+
+                    //var idx = 0
+                    //for(idx <- 0 until text.length)
+                    while (idx < text.length){
+                        if(ner(idx).toString == "\"PERSON\""){
+                            people = people + text(idx).toString
+                        } else if(ner(idx).toString == "\"LOCATION\""){
+                            locations = locations + text(idx).toString
+                        } else if(ner(idx).toString == "\"ORGANIZATION\""){
+                            organizations = organizations + text(idx).toString
+                        }
+                        idx = idx + 1
+                    }   
+                })
+            })
+            
+            
+          
+            // pass all sentences to Named Entity Recognition API
+            
+            // convert to list
+            val people_list = "[" + people.toList.mkString(",")+"]"
+            val locations_list = "[" + locations.toList.mkString(",")+"]"
+            val organizations_list = "[" + organizations.toList.mkString(",")+"]"
+            val new_sentence_list = "[" + sentence_list.map(x=>"\"" + x.filter(_ >= ' ') + "\"").mkString(",") + "]"
+           
+            // create a new document
+            val post_Data = s"""{"id":"${filename}","name":"${name}","url":"${url}","catchphrase":"${catchphrase.toString.filter(_ >= ' ')}","sentence":${new_sentence_list},"person":${people_list},"location":${locations_list},"organization":${organizations_list}}"""
+            val new_document_result = Http("http://localhost:9200/legal_idx/cases/"+filename+"?pretty").postData(post_Data).method("PUT").header("Content-Type", "application/json").option(HttpOptions.readTimeout(10000)).asString
+
+        })
+    }
+    
+    
+    
+    
+    // get list of files in a directory path
+    def getListOfFiles(dir: String):List[File] = {
+        val d = new File(dir)
+        if (d.exists && d.isDirectory) {
+            d.listFiles.filter(_.isFile).toList
+        } else {
+            List[File]()
+        }
+    }
+
+
+    //new entity
+    
+    //Word Process Function
+    def wordProcess() {
+        val newEn:Set[String] = Set()
+        reutrn newEn
+    }
+
+    
+
+
+}
